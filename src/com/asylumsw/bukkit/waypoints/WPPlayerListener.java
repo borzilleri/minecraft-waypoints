b7d1a1c7349e86d06b0017597f94792117cb2af7
Jonathan Borzilleri <jonathan.borzilleri@gmail.com>
2011-02-22 19:00:54 -0800

    Update to bukkit build jenkins-407, bugfixes
    - Fix some issues with going down unintended code paths on errors in
    	some gate commands

--- src/com/asylumsw/bukkit/waypoints/WPPlayerListener.java
+++ src/com/asylumsw/bukkit/waypoints/WPPlayerListener.java
@@ -10,6 +10,7 @@ import org.bukkit.entity.Player;
  * @author jonathan
  */
 public class WPPlayerListener extends PlayerListener {
+
 	private final Waypoints plugin;
 
 	public WPPlayerListener(Waypoints instance) {
@@ -25,50 +26,54 @@ public class WPPlayerListener extends PlayerListener {
 	 */
 	@Override
 	public void onPlayerCommand(PlayerChatEvent event) {
-		if( event.isCancelled() ) return;
+		if (event.isCancelled()) {
+			return;
+		}
 
 		String[] split = event.getMessage().split(" ");
 		String command = split[0];
 		Player player = event.getPlayer();
 
-		if( command.equalsIgnoreCase("/home")) {
+		if (command.equalsIgnoreCase("/home")) {
 			Homes.sendPlayerHome(player, false, false);
 			event.setCancelled(true);
 		}
-		else if( command.equalsIgnoreCase("/sethome") ) {
+		else if (command.equalsIgnoreCase("/sethome")) {
 			Homes.setHomePoint(player);
 			event.setCancelled(true);
 		}
-		else if( command.equalsIgnoreCase("/unsethome") ) {
+		else if (command.equalsIgnoreCase("/unsethome")) {
 			Homes.unsetHomePoint(player);
 			event.setCancelled(true);
 		}
-		else if( command.equalsIgnoreCase("/gates") ) {
+		else if (command.equalsIgnoreCase("/gates")) {
 			Gates.listPlayerGates(player);
 			event.setCancelled(true);
 		}
-		else if( command.equalsIgnoreCase("/gate") ) {
-			if( split.length < 2 ) {
-				player.sendMessage(ChatColor.RED+"Error: Must supply gate name.");
+		else if (command.equalsIgnoreCase("/gate")) {
+			if (split.length < 2) {
+				player.sendMessage(ChatColor.RED + "Error: Must supply gate name.");
 			}
-			Gates.debug = split.length >= 3 && player.isOp();
-			Gates.override = split.length >= 4 && player.isOp();
-			Gates.sendPlayerToGate(player, split[1]);
+			else {
+				Gates.debug = split.length >= 3 && player.isOp();
+				Gates.override = split.length >= 4 && player.isOp();
+				Gates.sendPlayerToGate(player, split[1]);
+			}
 			event.setCancelled(true);
 		}
-		else if( command.equalsIgnoreCase("/activategate") ) {
-			if( split.length < 2 ) {
-				player.sendMessage(ChatColor.RED+"Error: Must supply gate name.");
+		else if (command.equalsIgnoreCase("/activategate")) {
+			if (split.length < 2) {
+				player.sendMessage(ChatColor.RED + "Error: Must supply gate name.");
 			}
-			Gates.debug = split.length >= 3 && player.isOp();
-			Gates.override = split.length >= 4 && player.isOp();
-			Gates.activateGate(player, split[1]);
+			else {
+				Gates.debug = split.length >= 3 && player.isOp();
+				Gates.override = split.length >= 4 && player.isOp();
+				Gates.activateGate(player, split[1]);
+			}
 			event.setCancelled(true);
 		}
-		else if( command.equalsIgnoreCase("/deactivategate") ) {
-			player.sendMessage(ChatColor.RED+"Error: command not yet implemented.");
+		else if (command.equalsIgnoreCase("/deactivategate")) {
+			player.sendMessage(ChatColor.RED + "Error: command not yet implemented.");
 		}
 	}
-	
-
 }
